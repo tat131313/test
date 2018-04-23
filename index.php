621f8d99edf623e207671401cf38d395a1903a47
tat131313 <tatyapleshka@gmail.com>
2018-04-23 22:58:46 +0300

    commit

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
     function offset($arr,$n){
-        $count = count($arr); // it's comment
-        /*$newArr[0]=$arr[$count-2];
-        $newArr[1]=$arr[$count-1];*/
-        for($j = 0, $n1 = $n; $j < $n; $j++){   
-            $newArr[$j] = $arr[$count-$n1];
-            $n1--;
+        $count = count($arr); 
+        if($n < 0){
+           for($i = 0; $i < $count+$n; $i++){
+                $newArr[$i] = $arr[$i-$n];
+           }
+           for($j = 0, $n1=$n; $j<-$n; $j++){
+                $newArr[$count+$n1] = $arr[$j];
+                $n1++;
+           }
         }
-        for($i = 0; $i < $count-$n; $i++){            
-            $newArr[$i+$n]=$arr[$i]; 
+        else{
+            for($j = 0, $n1 = $n; $j < $n; $j++){   
+                $newArr[$j] = $arr[$count-$n1];
+                $n1--;
             }
+            for($i = 0; $i < $count-$n; $i++){            
+                $newArr[$i+$n]=$arr[$i]; 
+            }
+        }
         return $newArr;      
     }
     $arr = array(1,2,3,4,5,6,10,13);
-    $newArr = offset($arr,4);
-    var_dump($newArr); //[5,6,10,13,1,2,3,4]
+    $newArr = offset($arr,-3);
+    var_dump($newArr); //[4,5,6,10,13,1,2,3]
 ?>
