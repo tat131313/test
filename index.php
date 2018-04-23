db2c93a1e25d1cae96dc1f8c953b68821b0dd701
tat131313 <tatyapleshka@gmail.com>
2018-04-23 23:26:45 +0300

    commit

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
     function offset($arr,$n){
         $count = count($arr); 
-        if($n < 0){
+        if($n < 0){             //offset < 0
             $n = $count + $n;
+        }
+        while($n > $count){     //lenght of array > offset
+            $n -= $count;
         }
             for($j = 0, $n1 = $n; $j < $n; $j++){   
                 $newArr[$j] = $arr[$count-$n1];
@@ -14,6 +17,6 @@
         return $newArr;      
     }
     $arr = array(1,2,3,4,5,6,10,13);
-    $newArr = offset($arr,-3);
+    $newArr = offset($arr,9);
     var_dump($newArr); //[4,5,6,10,13,1,2,3]
 ?>
